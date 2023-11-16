277e1e7db7cef23b151bc02812334f51ef1ae8e0
adiv1kram <adityavikram250201@gmail.com>
2023-11-17 01:33:05 +0530

    flutter added json file

--- lib/screens/statsscreen.dart
+++ lib/screens/statsscreen.dart
@@ -18,31 +18,7 @@ class _StatScreenState extends State<StatScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Stack(
-          fit: StackFit.loose,
-          children: [
-            Column(
-              children: [
-                HorizontalCard(
-                  title: 'Blank',
-                  body: const Text('Blank Title'),
-                  backgroundColor: const Color.fromARGB(255, 167, 177, 167),
-                ),
-                const SizedBox(
-                  height: 12,
-                ),
-                HorizontalBarGraph(),
-                Row(
-                  children: [
-                    RadialGauge(),
-                  ],
-                )
-              ],
-            ),
-          ],
-        ),
-      ),
+      body: RadialGauge(),
     );
   }
 }
